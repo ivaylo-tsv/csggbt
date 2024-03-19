d1913329bd616cf800e09134d4f0aaec54446a32
Ivaylo Tsvetkov <ivaylo.tsvetkov@softwareone.com>
2024-03-19 21:41:03 +0200

    Add subnets

--- terraform/bootstrap/vpc.tf
+++ terraform/bootstrap/vpc.tf
@@ -2,8 +2,40 @@ resource "aws_vpc" "csggbt-vpc" {
   cidr_block = "10.0.0.0/24"
 
   tags = merge(
-    {"Name": "csggbt-vpc"},
+    { "Name" : "csggbt-vpc" },
     var.tags
   )
 }
 
+resource "aws_subnet" "csggbt-subnet-public" {
+  vpc_id                  = aws_vpc.csggbt-vpc.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "a"
+  map_public_ip_on_launch = "true"
+
+  tags = merge({
+    Name = "csggbt-subnet-public"},
+    var.tags)
+}
+
+resource "aws_subnet" "csggbt-subnet-private" {
+  vpc_id                  = aws_vpc.csggbt-vpc.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "b"
+  map_public_ip_on_launch = "false"
+
+  tags = merge({
+    Name = "csggbt-subnet-private"},
+    var.tags)
+}
+
+resource "aws_subnet" "csggbt-subnet-database" {
+  vpc_id                  = aws_vpc.csggbt-vpc.id
+  cidr_block              = "10.0.3.0/24"
+  availability_zone       = "c"
+  map_public_ip_on_launch = "false"
+
+  tags = merge({
+    Name = "csggbt-subnet-database"},
+    var.tags)
+}
