ff0a399f6b7aac7b612ce6bfe915dd565c753fef
Ivaylo Tsvetkov <ivaylo.tsvetkov@softwareone.com>
2024-03-19 21:11:02 +0200

    change tag

--- terraform/bootstrap/variables.tf
+++ terraform/bootstrap/variables.tf
@@ -2,7 +2,7 @@ variable "tags" {
   description = "Generic tags for the container specialty group green belt task project."
   type        = map(string)
   default = {
-    "project" = "csggt"
+    "project" = "csggbt"
     "region"  = "eu-west-1"
   }
 }
