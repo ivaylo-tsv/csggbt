184c3ba5c28ba5037cce2f6c68f1c0a6d6a8dd9b
Ivaylo Tsvetkov <ivaylo.tsvetkov@softwareone.com>
2024-03-20 09:21:44 +0200

    rt

--- terraform/bootstrap/rt.tf
+++ terraform/bootstrap/rt.tf
@@ -4,7 +4,7 @@ resource "aws_route_table" "csggbt-rt-public" {
   vpc_id = aws_vpc.csggbt-vpc.id
 
   route {
-    cidr_block = "10.0.0.0/24"
+    cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.csggbt-igw.id
   }
 
