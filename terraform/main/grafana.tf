14313f22a42186c6109b2e7a2f2628857fa3d4b1
Ivaylo Tsvetkov <ivaylo.tsvetkov@softwareone.com>
2024-03-22 11:48:14 +0200

    dass

--- terraform/main/grafana.tf
+++ terraform/main/grafana.tf
@@ -13,40 +13,40 @@
 #   namespace         = kubernetes_namespace.grafana.metadata[0].name
 #   dependency_update = true
 
-# #   values = []
+#   values = []
 
 #   lifecycle {
 #     ignore_changes = all
 #   }
 
-# #   set {
-# #     name  = "argo-cd.controller.serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
-# #     value = module.iam_assumable_role_argocd.iam_role_arn
-# #     type  = "string"
-# #   }
-# #   set {
-# #     name  = "argo-cd.applicationSet.serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
-# #     value = module.iam_assumable_role_argocd.iam_role_arn
-# #     type  = "string"
-# #   }
+#   set {
+#     name  = "argo-cd.controller.serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
+#     value = module.iam_assumable_role_argocd.iam_role_arn
+#     type  = "string"
+#   }
+#   set {
+#     name  = "argo-cd.applicationSet.serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
+#     value = module.iam_assumable_role_argocd.iam_role_arn
+#     type  = "string"
+#   }
 
-# #   set {
-# #     name  = "argo-cd.server.serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
-# #     value = module.iam_assumable_role_argocd.iam_role_arn
-# #     type  = "string"
-# #   }
+#   set {
+#     name  = "argo-cd.server.serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
+#     value = module.iam_assumable_role_argocd.iam_role_arn
+#     type  = "string"
+#   }
 
-# #   set {
-# #     name  = "argo-cd.server.serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
-# #     value = module.iam_assumable_role_argocd.iam_role_arn
-# #     type  = "string"
-# #   }
+#   set {
+#     name  = "argo-cd.server.serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
+#     value = module.iam_assumable_role_argocd.iam_role_arn
+#     type  = "string"
+#   }
 
-# #   set {
-# #     name  = "argocd-apps.applicationsets[0].additionalAnnotations.helm\\.sh/hook"
-# #     value = "post-install"
-# #     type  = "string"
-# #   }
+#   set {
+#     name  = "argocd-apps.applicationsets[0].additionalAnnotations.helm\\.sh/hook"
+#     value = "post-install"
+#     type  = "string"
+#   }
 # }
 
 
