ea483d553f1404c44555f57bee61e64ef82fd019
Ivaylo Tsvetkov <ivaylo.tsvetkov@softwareone.com>
2024-03-28 09:16:54 +0200

    changes

--- terraform/main/argo.tf
+++ terraform/main/argo.tf
@@ -5,19 +5,6 @@ resource "kubernetes_namespace" "argocd" {
   depends_on = [module.eks]
 }
 
-# resource "helm_release" "git_repo_secret" {
-#   depends_on = [module.eks]
-#   name = "csggbt-repo-privatekey"
-#   chart = "./helm/charts/csggbt-repo-privatekey"
-#   dependency_update = true
-
-#   values = []
-
-#   lifecycle {
-#     ignore_changes = all 
-#   }
-# }
-
 resource "helm_release" "argocd" {
 
   name              = "csggbt-argocd"
