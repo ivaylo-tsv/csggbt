e7db14788fee7ebcc20179876a913feda925c96a
Ivaylo Tsvetkov <ivaylo.tsvetkov@softwareone.com>
2024-03-28 09:52:18 +0200

    change

--- terraform/main/argo.tf
+++ terraform/main/argo.tf
@@ -18,3 +18,19 @@ resource "helm_release" "argocd" {
     ignore_changes = all
   }
 }
+
+
+resource "helm_release" "root-app" {
+  depends_on = [helm_release.argocd]
+
+  name              = "csggbt-root-app"
+  chart             = "./helm/charts/root-app"
+  namespace         = "default"
+  dependency_update = true
+
+  values = []
+
+  lifecycle {
+    ignore_changes = all
+  }
+}
