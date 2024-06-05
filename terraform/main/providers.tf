ea08c646b3222fe67d42ba1deacdda767a02bbb8
Ivaylo Tsvetkov <ivaylo.tsvetkov@softwareone.com>
2024-06-05 13:35:52 +0300

    updates

--- terraform/main/providers.tf
+++ terraform/main/providers.tf
@@ -24,6 +24,7 @@ provider "helm" {
     host                   = data.aws_eks_cluster.eks_cluster.endpoint
     cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks_cluster.certificate_authority.0.data)
     token                  = data.aws_eks_cluster_auth.aws_cluster_auth.token
+    config_path = "~/.kube/config"
   }
 }
 
